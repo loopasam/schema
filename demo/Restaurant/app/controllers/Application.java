fa2b9e15d3532fdfb7659c3bb04d873d53b7a40b
Samuel Croset <samuel.croset@gmail.com>
2012-09-03 17:12:27 +0100

    tuto

--- demo/Restaurant/app/controllers/Application.java
+++ demo/Restaurant/app/controllers/Application.java
@@ -10,7 +10,7 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+	List<Restaurant> restaurants = Restaurant.find("order by name desc").fetch();
+	render(restaurants);    
     }
-
 }
